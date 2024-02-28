0b03c2181905c335ca5d67b62432d7a131b29a2e
zacherikson <100091786+zacherikson@users.noreply.github.com>
2024-02-27 22:27:05 -0700

    more on uploads

--- workout.js
+++ workout.js
@@ -3,6 +3,7 @@ class Workout {
         this.updateHeadline();
         this.hideUploadInfo();
     }
+    workoutType;
 
     updateHeadline() {
         const headlineTitleEl = document.querySelector('.headlineTitle h1');
@@ -21,16 +22,40 @@ class Workout {
         document.getElementById('uploadContainer').style.display = 'none'
     }
 
-    setupRunBikeSwimUpload() {
-        document.getElementById('activityInfo').style.display = ''
+    runBikeSwimUploads() {
+        document.getElementById('activityInfo').style.display = '';
         document.getElementById('distanceActivity').style.display = '';
         document.getElementById('timeActivity').style.display = '';
         document.getElementById('dietActivity').style.display = 'none';
         document.getElementById('uploadContainer').style.display = ''
     }
 
+    setupRunUpload() {
+        this.workoutType = 'Run';
+        const titleEl = document.querySelector('.titleActivity b');
+        titleEl.textContent = 'Run Title';
+        this.runBikeSwimUploads();
+    }
+
+    setupBikeUpload() {
+        this.workoutType = 'Bike';
+        const titleEl = document.querySelector('.titleActivity b');
+        titleEl.textContent = 'Bike Title';
+        this.runBikeSwimUploads();
+    }
+
+    setupSwimUpload() {
+        this.workoutType = 'Swim';
+        const titleEl = document.querySelector('.titleActivity b');
+        titleEl.textContent = 'Swim Title';
+        this.runBikeSwimUploads();
+    }
+
     setupGymUpload() {
-        document.getElementById('activityInfo').style.display = ''
+        this.workoutType = 'Gym';
+        const titleEl = document.querySelector('.titleActivity b');
+        titleEl.textContent = 'Gym Title';
+        document.getElementById('activityInfo').style.display = '';
         document.getElementById('distanceActivity').style.display = 'none';
         document.getElementById('timeActivity').style.display = '';
         document.getElementById('dietActivity').style.display = 'none';
@@ -38,11 +63,56 @@ class Workout {
     }
 
     setupDietUpload() {
-        document.getElementById('activityInfo').style.display = ''
+        this.workoutType = 'Diet';
+        const titleEl = document.querySelector('.titleActivity b');
+        titleEl.textContent = 'Diet Title';
+        document.getElementById('activityInfo').style.display = '';
         document.getElementById('distanceActivity').style.display = 'none';
         document.getElementById('timeActivity').style.display = 'none';
         document.getElementById('dietActivity').style.display = '';
         document.getElementById('uploadContainer').style.display = ''
+    }
+
+    upload() {
+        let title = document.getElementById('title').value;
+        let notes = document.getElementById('note').value; 
+        let activityData = {}
+
+        if (this.workoutType === 'Run' || this.workoutType === 'Bike' ||
+        this.workoutType === 'Swim') {
+            let distance = document.getElementById('distance').value;
+            let time = document.getElementById('time').value;
+            activityData = {
+                type: this.workoutType,
+                title: title,
+                distance: distance,
+                time: time,
+                notes: notes
+            };
+        }
+        if (this.workoutType === 'Gym'){
+            let time = document.getElementById('time').value;
+            activityData = {
+                type: this.workoutType,
+                title: title,
+                time: time,
+                notes: notes
+            };
+        }
+        if (this.workoutType === 'Diet') {
+            let diet = document.getElementById('diet').value;
+            activityData = {
+                type: this.workoutType,
+                title: title,
+                dietRating: diet,
+                notes: notes
+            };
+        }
+
+        let activityDataJSON = JSON.stringify(activityData);
+
+        localStorage.setItem('activityData', activityDataJSON);
+        
     }
 
 }
