434272d06bf11e2ed2b83004107a555f0cc9b3a6
zacherikson <100091786+zacherikson@users.noreply.github.com>
2024-02-28 09:56:45 -0700

    weekly goals

--- history.js
+++ history.js
@@ -40,7 +40,8 @@ class History {
             const activityEl = document.createElement('div');
             activityEl.className = 'activity';
             let activityData = activities[activity].activityData
-            activityEl.innerHTML = `
+            if (activityData.type === 'Run') {
+                activityEl.innerHTML = `
                 <div class="workout">
                     <div><b>${activityData.title}</b></div>
                     <table>
@@ -60,8 +61,89 @@ class History {
                     <p>${activityData.notes || ''}</p>
                 </div>
                 <hr>`;
-            historyEl.appendChild(activityEl); 
-        };
+            historyEl.appendChild(activityEl);
+            }
+            if (activityData.type === 'Bike') {
+                activityEl.innerHTML = `
+                <div class="workout">
+                    <div><b>${activityData.title}</b></div>
+                    <table>
+                        <tr>
+                            <th>Type</th>
+                            <th>Distance</th>
+                            <th>Time</th>
+                        </tr>
+                        <tr>
+                            <td>${activityData.type || 'N/A'}</td>
+                            <td>${activityData.distance || 'N/A'}</td>
+                            <td>${activityData.time || 'N/A'}</td>
+                        </tr>
+                    </table>
+                    <p>${activityData.notes || ''}</p>
+                </div>
+                <hr>`;
+            historyEl.appendChild(activityEl);
+            }
+            if (activityData.type === 'Swim') {
+                activityEl.innerHTML = `
+                <div class="workout">
+                    <div><b>${activityData.title}</b></div>
+                    <table>
+                        <tr>
+                            <th>Type</th>
+                            <th>Distance</th>
+                            <th>Time</th>
+                        </tr>
+                        <tr>
+                            <td>${activityData.type || 'N/A'}</td>
+                            <td>${activityData.distance || 'N/A'}</td>
+                            <td>${activityData.time || 'N/A'}</td>
+                        </tr>
+                    </table>
+                    <p>${activityData.notes || ''}</p>
+                </div>
+                <hr>`;
+            historyEl.appendChild(activityEl);
+            }
+            if (activityData.type === 'Gym') {
+                activityEl.innerHTML = `
+                <div class="workout">
+                    <div><b>${activityData.title}</b></div>
+                    <table>
+                        <tr>
+                            <th>Type</th>
+                            <th>Time</th>
+                        </tr>
+                        <tr>
+                            <td>${activityData.type || 'N/A'}</td>
+                            <td>${activityData.time || 'N/A'}</td>
+                        </tr>
+                    </table>
+                    <p>${activityData.notes || ''}</p>
+                </div>
+                <hr>`;
+            historyEl.appendChild(activityEl);
+            }
+            if (activityData.type === 'Diet') {
+                activityEl.innerHTML = `
+                <div class="workout">
+                    <div><b>${activityData.title}</b></div>
+                    <table>
+                        <tr>
+                            <th>Type</th>
+                            <th>Diet</th>
+                        </tr>
+                        <tr>
+                            <td>${activityData.type || 'N/A'}</td>
+                            <td>${activityData.dietRating || 'N/A'}</td>
+                        </tr>
+                    </table>
+                    <p>${activityData.notes || ''}</p>
+                </div>
+                <hr>`;
+            historyEl.appendChild(activityEl);
+            }
+        }
     }
 }
 
