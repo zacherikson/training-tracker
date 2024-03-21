263a35b4acc22a56828955319ccc55245f2b0765
zacherikson <100091786+zacherikson@users.noreply.github.com>
2024-03-21 10:59:14 -0600

    database

---
+++ database.js
@@ -0,0 +1,14 @@
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+
+const client = new MongoClient(url);
+const db = client.db('rental');
+
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
