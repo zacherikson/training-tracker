6b7d338b4fb5e7922f380c9e3bd68a28d6f60f4d
zacherikson <100091786+zacherikson@users.noreply.github.com>
2024-03-22 12:15:02 -0600

    making database changes

--- database.js
+++ database.js
@@ -1,10 +1,16 @@
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
-
 const client = new MongoClient(url);
-const db = client.db('rental');
+const db = client.db('startup');
+const userCollection = db.collection('user');
+const workoutCollection = db.collection('workout');
+const goalCollection = db.collection('goal');
 
+// This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
   await client.connect();
   await db.command({ ping: 1 });
@@ -12,3 +18,47 @@ const db = client.db('rental');
   console.log(`Unable to connect to database with ${url} because ${ex.message}`);
   process.exit(1);
 });
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+  // Hash the password before we insert it into the database
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await userCollection.insertOne(user);
+
+  return user;
+}
+
+function addWorkout(workout) {
+  workoutCollection.insertOne(workout);
+}
+
+function getWorkouts() {
+  const query = { workout: 1 }; //{ $gt: 0, $lt: 900 }
+  // const options = {
+  //   sort: { score: -1 },
+  //   limit: 10,
+  // };
+  const cursor = workoutCollection.find(query);//, options
+  return cursor.toArray();
+}
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  createUser,
+  addWorkout,
+  getWorkouts,
+};
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const express = require('express');
+const DB = require('./database.js');
+
 const app = express();
+
+const authCookieName = 'token';
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
@@ -7,20 +13,87 @@ const port = process.argv.length > 2 ? process.argv[2] : 3000;
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
 // Serve up the front-end static content hosting
 app.use(express.static('public'));
+
+// Trust headers that are forwarded from the proxy so we can determine IP addresses
+app.set('trust proxy', true);
 
 // Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
+// CreateAuth token for a new user
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await DB.getUser(req.body.email)) {
+      res.status(409).send({ msg: 'Existing user' });
+    } else {
+      const user = await DB.createUser(req.body.email, req.body.password);
+  
+      // Set the cookie
+      setAuthCookie(res, user.token);
+  
+      res.send({
+        id: user._id,
+      });
+    }
+  });
+
+  // GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await DB.getUser(req.body.email);
+    if (user) {
+      if (await bcrypt.compare(req.body.password, user.password)) {
+        setAuthCookie(res, user.token);
+        res.send({ id: user._id });
+        return;
+      }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+  });
+
+// DeleteAuth token if stored in cookie
+apiRouter.delete('/auth/logout', (_req, res) => {
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+  });
+
+// GetUser returns information about a user
+apiRouter.get('/user/:email', async (req, res) => {
+    const user = await DB.getUser(req.params.email);
+    if (user) {
+      const token = req?.cookies.token;
+      res.send({ email: user.email, authenticated: token === user.token });
+      return;
+    }
+    res.status(404).send({ msg: 'Unknown' });
+  });
+
+// secureApiRouter verifies credentials for endpoints
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
+
 // GetWorkouts
-apiRouter.get('/workouts', (_req, res) => {
+secureApiRouter.get('/workouts', async (_req, res) => {
+    const workouts = await DB.getWorkouts();
   res.send(workouts);
 });
 
 // GetRunGoal
-apiRouter.get('/runGoals', (_req, res) => {
+secureApiRouter.get('/runGoals', (_req, res) => {
     if (runGoal === ''){
         res.send('0');
     } else {
@@ -29,7 +102,7 @@ apiRouter.get('/runGoals', (_req, res) => {
 });
 
 // GetBikeGoal
-apiRouter.get('/bikeGoals', (_req, res) => {
+secureApiRouter.get('/bikeGoals', (_req, res) => {
     if (bikeGoal === ''){
         res.send('0');
     } else {
@@ -38,7 +111,7 @@ apiRouter.get('/bikeGoals', (_req, res) => {
 });
 
 // GetSwimGoal
-apiRouter.get('/swimGoals', (_req, res) => {
+secureApiRouter.get('/swimGoals', (_req, res) => {
     if (swimGoal === ''){
         res.send('0');
     } else {
@@ -47,7 +120,7 @@ apiRouter.get('/swimGoals', (_req, res) => {
 });
 
 // GetGymGoal
-apiRouter.get('/gymGoals', (_req, res) => {
+secureApiRouter.get('/gymGoals', (_req, res) => {
     if (gymGoal === ''){
         res.send('0');
     } else {
@@ -56,7 +129,7 @@ apiRouter.get('/gymGoals', (_req, res) => {
 });
 
 // GetDietGoal
-apiRouter.get('/dietGoals', (_req, res) => {
+secureApiRouter.get('/dietGoals', (_req, res) => {
     if (dietGoal === ''){
         res.send('0');
     } else {
@@ -65,41 +138,50 @@ apiRouter.get('/dietGoals', (_req, res) => {
 });
 
 // SubmitWorkout
-apiRouter.post('/workout', (req, res) => {
-    workouts = updateWorkouts(req.body, workouts);
+secureApiRouter.post('/workout', async (req, res) => {
+    const workout = { ...req.body, ip: req.ip };
+    await DB.addWorkout(workout);
+    const workouts = await DB.getWorkouts();
   res.send(workouts);
+//     workouts = updateWorkouts(req.body, workouts);
+//   res.send(workouts);
 });
 
 // SubmitGoal
 let runGoal = '';
-apiRouter.post('/runGoal', (req, res) => {
+secureApiRouter.post('/runGoal', (req, res) => {
     runGoal = req.body.runGoal;
     res.send(runGoal);
 });
 
 let bikeGoal = '';
-apiRouter.post('/bikeGoal', (req, res) => {
+secureApiRouter.post('/bikeGoal', (req, res) => {
     bikeGoal = req.body.bikeGoal;
     res.send(bikeGoal);
 });
 
 let swimGoal = '';
-apiRouter.post('/swimGoal', (req, res) => {
+secureApiRouter.post('/swimGoal', (req, res) => {
     swimGoal = req.body.swimGoal;
     res.send(swimGoal);
 });
 
 let gymGoal = '';
-apiRouter.post('/gymGoal', (req, res) => {
+secureApiRouter.post('/gymGoal', (req, res) => {
     gymGoal = req.body.gymGoal;
     res.send(gymGoal);
 });
 
 let dietGoal = '';
-apiRouter.post('/dietGoal', (req, res) => {
+secureApiRouter.post('/dietGoal', (req, res) => {
     dietGoal = req.body.dietGoal;
     res.send(dietGoal);
 });
+
+// Default error handler
+app.use(function (err, req, res, next) {
+    res.status(500).send({ type: err.name, message: err.message });
+  });
 
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
@@ -110,10 +192,19 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+    res.cookie(authCookieName, authToken, {
+      secure: true,
+      httpOnly: true,
+      sameSite: 'strict',
+    });
+  }
+
 // The workouts are saved in memory and disappear whenever the service is restarted.
-let workouts = [];
-function updateWorkouts(newWorkout, workouts) {
-    workouts.push(newWorkout);
-    return workouts;
-}
+// let workouts = [];
+// function updateWorkouts(newWorkout, workouts) {
+//     workouts.push(newWorkout);
+//     return workouts;
+// }
 
